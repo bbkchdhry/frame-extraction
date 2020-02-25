b9503d4f3dfa8a0e9493edb6eb8d74d3d51bc418
Bibek Chaudhary <bbkchdhry@gmail.com>
2020-02-25 09:58:48 +0545

    removing ts files

--- ts_generator/generate.py
+++ ts_generator/generate.py
@@ -12,43 +12,14 @@ stream3 = os.getenv("stream_abpnews")
 stream4 = os.getenv("stream_radio_kantipur")
 stream_home = os.getenv("stream_home")
 
-pid = 0
 ts_list = []
 
 
 def run(stream_dir, stream_name, stream):
-    cmd = """ffmpeg -y -i {0} -c:v libx264 -s:v 960x540 -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_list_size 0 -hls_time 10 -hls_allow_cache 0 -b:v 168k \
+    cmd = """ffmpeg -y -i {0} -f hls -s:v 960x540 -hls_list_size 5 -hls_time 5 -hls_flags delete_segments -b:v 168k \
     -b:a 125k -maxrate 202k -hls_segment_filename {1}/{2}_%04d.ts -strict -2 {3}/{4}.m3u8""".format(stream, stream_dir, stream_name, stream_dir, stream_name)
     p = sp.Popen(cmd, stdout=sp.PIPE, shell=True)
-    global pid
-    pid = p.pid
     p.communicate()
-
-
-def remove_previous_ts():
-    try:
-        if pid > 0:
-            while pid > 0:
-                with open(stream_home+"/kantipur.m3u8", "r") as f:
-                    lines = f.readlines()
-                    ts_list.append(lines[-1].replace('\n',''))
-
-                print(ts_list)
-                if len(ts_list) == 3:
-                    with open(stream_home+"/kantipur.m3u8", "w") as f:
-                        line = f.read()
-                        if line == ts_list[0]:
-                            print(line)
-
-                time.sleep(10)
-        else:
-            print("pid not > 0")
-            time.sleep(2)
-            remove_previous_ts()
-    except FileNotFoundError:
-        print("file not found")
-        time.sleep(2)
-        remove_previous_ts()
 
 
 if __name__ == '__main__':
